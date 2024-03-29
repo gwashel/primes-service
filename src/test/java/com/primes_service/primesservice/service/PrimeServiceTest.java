0109298d1b76fd1ce1d66254d3881159fc33ae29
GioWashel <giowashel@gmail.com>
2024-03-28 23:23:41 -0400

    first deployment

--- src/test/java/com/primes_service/primesservice/service/PrimeServiceTest.java
+++ src/test/java/com/primes_service/primesservice/service/PrimeServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeServiceTest {
-    PrimeService primeService = new PrimeService();
+    PrimesService primeService = new PrimesService();
 
 
     @Test
